4bfa496159c8664951a72c7eaa2bb8d982468ca8
Relliko <apolywka@gmail.com>
2021-09-21 19:11:37 -0400

    Updates

---
+++ precache-manifest.5273d81fa50d79cf3e46955c8cf9c110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ca96d5250364e56494e22df2897781",
+    "url": "/campus-paths/static/media/campus_map.11ca96d5.jpg"
+  },
+  {
+    "revision": "9091292a217037d0fba1",
+    "url": "/campus-paths/static/js/runtime~main.4b10f28a.js"
+  },
+  {
+    "revision": "6141b0dd7117b5208946",
+    "url": "/campus-paths/static/js/main.c1037198.chunk.js"
+  },
+  {
+    "revision": "232d088fab411b0105d9",
+    "url": "/campus-paths/static/js/2.7a25a887.chunk.js"
+  },
+  {
+    "revision": "6141b0dd7117b5208946",
+    "url": "/campus-paths/static/css/main.14a5d2a5.chunk.css"
+  },
+  {
+    "revision": "0818b067f2a3961972764d187c8cd323",
+    "url": "/campus-paths/index.html"
+  }
+];
